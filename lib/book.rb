b801b1b5c82c74c0fba11440898a565c1fc45929
Riccardo <riccardo_london@live.it>
2019-03-13 14:33:25 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,29 @@
 class Book
 
-  def initialized(title)]
+  def initialized(title, author, page_count, genre)
     @title = title
-  end
+    @author = author
+    @page_count = page_count
+    @genre = genre
+end
+  def title=(banana)
+    @title = banana
+end
+def author=(apple)
+  @author = apple  
+end
 
-  def title=(new_title)
-    @title = new_title
-  end
+def page_count=(num)
+  @page_count = num
+end
 
-  def author=(new_author)
-    @author = new_author
+def genre=(new_gen)
+  @genre = new_gen
+end
+
+def turn_page
+      puts "Flipping the page...wow, you read fast!"
+end
+
 
 end
