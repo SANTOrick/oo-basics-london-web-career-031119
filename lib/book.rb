52208e1a84b12b415b3a1a069f516eb19593a2bf
Riccardo <riccardo_london@live.it>
2019-03-13 14:47:29 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,29 +1,13 @@
 class Book
-
-  def initialized
-    attr_accessor :author, :genre, :page_count
-    attr_reader :title, :turn_page
-end
-
-def title
-  @title 
-end
-
-def author=(apple)
-  @author = apple
-end
-
-def page_count=(num)
-  @page_count = num
-end
-
-def genre=(new_gen)
-  @genre = new_gen
-end
-
-def turn_page
-      puts "Flipping the page...wow, you read fast!"
-end
-
-
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
 end
