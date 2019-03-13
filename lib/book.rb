28f88c2b4ba7405e9a157c28b58c2f5fdb803022
Riccardo <riccardo_london@live.it>
2019-03-13 14:25:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
 
+  def initialized(title)]
+    @title = title
+  end
 
+  def title=(new_title)
+    @title = new_title
+  end
+
+  def author=(new_author)
+    @author = new_author
+
+end
